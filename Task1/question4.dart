6ef44b777e05484832aa204370e9b8a513dbe500
kajal1707 <97467080+kajal1707@users.noreply.github.com>
2022-02-26 17:26:06 +0530

    question4.dart

---
+++ Task1/question4.dart
@@ -0,0 +1,13 @@
+//4. WAP to perform mathematical operations
+
+void main() {
+  sum(6,2);
+   sub(6,2);
+   mul(6,2);
+   div(6,2);
+  
+}
+ void sum(int a,int b)=>print('sum=${a+b}');
+ void sub(int a,int b)=>print('sub=${a-b}');
+ void mul(int a,int b)=>print('mul=${a*b}');
+ void div(int a,int b)=>print('div=${a/b}');
