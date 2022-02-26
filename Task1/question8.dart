52dc94c769858f1cb4014fc747a01f09a0453706
kajal1707 <97467080+kajal1707@users.noreply.github.com>
2022-02-26 17:31:19 +0530

    question8.dart

---
+++ Task1/question8.dart
@@ -0,0 +1,6 @@
+//8.WAP maximum of three no. using ter. operator
+void main()
+{
+  var a=10,b=23,c=14;
+  ((a>b)&&(a>c))?print('$a is maximum' ):(b>c)?print('$b is maximum'):print('$c is maximum');
+}
