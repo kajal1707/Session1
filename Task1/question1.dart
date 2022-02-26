cc94b4396e4b424de9925413f2da9adb721e460c
kajal1707 <97467080+kajal1707@users.noreply.github.com>
2022-02-26 17:23:10 +0530

    question1.dart

---
+++ Task1/question1.dart
@@ -0,0 +1,10 @@
+//1.WAP to check number is divisible b y 5 or not
+void main ()
+{
+  var num=232;
+  if(num%5==0)
+    print('Number is divisible by 5');
+  else
+    print('Number is not divisible by 5');
+    
+}
