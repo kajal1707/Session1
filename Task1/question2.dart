13f572293052516f4f27b5f10aaba5b35d7d4885
kajal1707 <97467080+kajal1707@users.noreply.github.com>
2022-02-26 17:24:03 +0530

    question2.dart

---
+++ Task1/question2.dart
@@ -0,0 +1,15 @@
+//2. WAP to check no. is even or odd
+void main() 
+{
+ print (eo(num));
+  
+  
+}
+ int? eo(var num)
+{
+  int num=22;
+  if(num%2==0)
+    print('num is even');
+  else
+    print('num is odd');
+}
